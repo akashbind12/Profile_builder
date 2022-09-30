22ebb8c3cfa2cdc6f09a5f274234eff52f1c7914
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-09-30 14:26:24 +0530

    half UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState("Akash")
+  const [data, setData] = useState({
+    "name": "",
+    "location": "",
+    "school": "",
+    "major": "",
+    "occupation": "",
+    "religiousBackgrpund": "",
+    "reasonForMeeting" : "",
+  })
 
   const handlechange = (e) => {
-    setName(e.target.value)
+    setData({
+      ...data,
+      [e.target.name] : e.target.value
+    })
   }
 
   return (
     <div className="App">
-      <h1>Profile Builder</h1>
+      <h1 className='heading'>Bio Generator</h1>
       <div className='container'>
-        <div>
-          <div>Enter your name</div>
-          <input value={name} type="text" placeholder='Enter Your name' onChange={handlechange} />
+        <div className='option-div'>
+          <h1 className='heading'>Options</h1>
+          
+          <div className='option-div-child'>
+            <div>Name :</div>
+            <input name='name' value={data.name} type="text"  onChange={handlechange} />
+            <div>Gender :</div>
+            <select name="" id="">
+              <option value="male">male</option>
+              <option value="female">female</option>
+            </select>
+            <button>Random Name</button>
+          </div>
+
+          <div className='option-div-child'>
+            <input type="checkbox" />
+            <div>Location :</div>
+            <input name='location' value={data.location}  type="text"  onChange={handlechange} />
+            <button>Random Location</button>
+          </div>
+
+          <div className='option-div-child'>
+            <input type="checkbox" />
+            <div>School :</div>
+            <input name='school' value={data.school}  type="text"  onChange={handlechange} />
+            <button>Random School</button>
+          </div>
+          <div style={{marginTop : "0"}} className='option-div-child'>
+            <div>Major :</div>
+            <input name='major' value={data.major}  type="text"  onChange={handlechange} />
+            <button>Random Major</button>
+          </div>
+
         </div>
-        <div>
-          <div>my name is {name }</div>
+
+        {/* ------- result div-----------------------*/}
+        <div className='result-div'>
+        <h1 className='heading'>Result</h1>
+          <div className='option-div-child'>{data.name} is from {data.location}  and moved to the Utah Valley for school.
+            She is studying to be a speech pathologist at the University of Utah. She was raised without any religion in the home.
+            For most of her life, she has never had any interest in spiritual things.
+            She has recently lost a loved one, and is looking for ways to find peace during this difficult time.</div>
         </div>
        </div>
     </div>
