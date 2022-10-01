c34167c6bcffdeb084d4830302e12aa42238aeb4
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-10-01 22:28:30 +0530

    ramdom button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 
+const RandomData = {
+  "Name": ["Amanda", "Hannah", "Rachel", "Josh", "Maggie", "Taylor"],
+  "Location": ["Mumbai", "NaviMumbai", "bangluru", "hydrabad", "chennai"],
+  "School": ["BYU", "Utah State", "the University of Utah", "Utah State", "UVU"],
+  "Major": ["Animation", "Family Science", "Information Technology", "Math Education", "Communication Disorders"],
+  "Occupation": ["journalist for a local newspaper", "journalist for a local newspaper",
+    "TA at [PossessivePronoun] school", "web developer", "civil Engineer"],
+  "ReligiousBackgrpund": ["atheist, and has no experience with spiritual things",
+    "in a nontraditional family thatwas very focused on spirituality and connecting with the inner being through meditation and nature",
+    "Catholic but would consider [ObjectPronoun]self to be agnostic right now",
+    "Catholic", "never discussing religion in [PossessivePronoun] home"],
+  "ReasonForMeeting": ["is looking to develop a more personal relationship with God",
+    "really enjoyed [PossessivePronoun] first meeting with the missionaries (the Restoration) and felt good about the Book of Mormon when [SubjectPronoun] prayed about it",
+    "often worries that God is disappointed in [ObjectPronoun] for not following him better and wants to know how to change",
+  "harbors feelings of resentment towards God because of difficult trials in [PossessivePronoun] life, but is willing to give religion and spirituality another try"]
+  
+}
 
 function App() {
   const [data, setData] = useState({
@@ -14,7 +31,7 @@ function App() {
   })
 
   const [gender, setGender] = useState("He")
-  
+  let [count, setCount] = useState(0)
   const handleGender = (e) => {
     console.log("gender:", e.target.value)
     e.target.value == "male" ? setGender("He") : setGender("She")
@@ -25,6 +42,19 @@ function App() {
       ...data,
       [e.target.name] : e.target.value
     })
+  }
+  
+ 
+  const handleRandomdata = (key,value) => {
+    console.log(key,RandomData.Name[count],count,)
+    if (count > 4) {
+      count = 0
+    }
+    setData({
+      ...data,
+      [key] : value[count]
+    })
+    setCount(count+1);
   }
 
   return (
@@ -42,33 +72,33 @@ function App() {
               <option value="male"> male </option>
               <option value="female"> female </option>
             </select>
-            <button>Random Name</button>
+            <button onClick={() => handleRandomdata("name",RandomData.Name)} >Random Name</button>
           </div>
 
           <div className='option-div-child'>
             <input type="checkbox" />
             <div> Location : </div>
             <input name='location' value={data.location}  type="text"  onChange={handlechange} />
-            <button> Random Location </button>
+            <button onClick={() => handleRandomdata("location",RandomData.Location)} > Random Location </button>
           </div>
 
           <div style={{marginBottom : "0"}} className='option-div-child'>
             <input type="checkbox" />
             <div> School : </div>
             <input name='school' value={data.school}  type="text"  onChange={handlechange} />
-            <button>Random School</button>
+            <button onClick={() => handleRandomdata("school",RandomData.School)} >Random School</button>
           </div>
           <div style={{marginTop : "0"}} className='option-div-child'>
             <div> Major :</div>
             <input name='major' value={data.major}  type="text"  onChange={handlechange} />
-            <button> Random Major </button>
+            <button onClick={() => handleRandomdata("major",RandomData.Major)}> Random Major </button>
           </div>
 
           <div className='option-div-child'>
             <input type="checkbox" />
             <div> Occupation : </div>
             <input name='occupation' value={data.occupation}  type="text"  onChange={handlechange} />
-            <button> Random Occupation </button>
+            <button onClick={() => handleRandomdata("occupation",RandomData.Occupation)}  > Random Occupation </button>
           </div>
 
           <div style={{display : "block", textAlign : "left"}} className='option-div-child'>
@@ -77,7 +107,7 @@ function App() {
             <div> Religious Backgrpund : </div>
             </div>
             <textarea style={{width : "95%", height : "60px" , margin : "10px"}} name='religiousBackgrpund' value={data.religiousBackgrpund}  type="text"  onChange={handlechange} />
-            <button> Random Religion </button>
+            <button onClick={() => handleRandomdata("religiousBackgrpund",RandomData.ReligiousBackgrpund)} > Random Religion </button>
           </div>
 
           <div style={{display : "block", textAlign : "left"}} className='option-div-child'>
@@ -86,12 +116,12 @@ function App() {
             <div> Reason for meeting with missionaries: </div>
             </div>
             <textarea style={{width : "95%", height : "60px" , margin : "10px"}} name='reasonForMeeting' value={data.reasonForMeeting}  type="text"  onChange={handlechange} />
-            <button> Restoration </button>
-            <button> Plan of Salvation </button>
-            <button> Gospel of Christ </button>
-            <button> Law of Chastity </button>
-            <button> Word of Wisdom </button>
-            <button> Any Lesson </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)} > Restoration </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)}> Plan of Salvation </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)}> Gospel of Christ </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)}> Law of Chastity </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)}> Word of Wisdom </button>
+            <button onClick={() => handleRandomdata("reasonForMeeting",RandomData.ReasonForMeeting)}cc> Any Lesson </button>
           </div>
 
         </div>
